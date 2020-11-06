e16180e1bdd45d749f447c09e956023bc6069b80
Rgaliant <ryan@getdirect.io>
2020-11-06 12:01:44 -0700

    --allow-empty

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,6 @@
 class BookingsController < ApplicationController
   def index
     # Edit the index method on the bookings_controller to accept a param of filter if the param is not nil, and to return the correct bookings.
-    bookings = Booking.all
 
     render json: bookings, status: 200
   end
